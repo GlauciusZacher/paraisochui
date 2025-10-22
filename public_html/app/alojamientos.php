ac4b6571aef5cef49849f14825227a1b6f0f2a8f
GlauciusZacher <glaucius.zacher@gmail.com>
2025-10-22 10:26:23 -0300

    Initial commit

---
+++ public_html/app/alojamientos.php
@@ -0,0 +1,41 @@
+<main>
+    <section class="alojamientos">
+        <div class="alojamientos-module">
+            <div><img src="assets/img/slider/slider-1920-03.jpg" alt="Alojamientos en Paraíso Chuí"></div>
+            <div>
+                <h1>Bienvenidos a Paraíso Chuí</h1>
+                <p>Su refugio ideal en Barra do Chuí, Brasil</p>
+                <p>Descubra nuestras cómodas y acogedoras opciones de alojamiento diseñadas para brindarle una experiencia inolvidable en Paraíso Chuí. Ya sea que viaje en pareja, en familia o con amigos, tenemos el lugar perfecto para usted.</p>
+            </div>
+        </div>
+        <div class="alojamientos-module">
+            <div><img src="assets/img/slider/slider-1920-03.jpg" alt="Alojamientos en Paraíso Chuí"></div>
+            <div>
+                <h1>Monoambientes para 2 personas</h1>
+                <p>Perfectos para una escapada romántica o un viaje en pareja, nuestros monoambientes están equipados con todas las comodidades que necesita para una estancia agradable.</p>
+            </div>
+        </div>
+        <div class="alojamientos-module">
+            <div><img src="assets/img/slider/slider-1920-03.jpg" alt="Alojamientos en Paraíso Chuí"></div>
+            <div>
+                <h1>Monoambientes para 2 personas</h1>
+                <p>Perfectos para una escapada romántica o un viaje en pareja, nuestros monoambientes están equipados con todas las comodidades que necesita para una estancia agradable.</p>
+            </div>
+        </div>
+        <div class="alojamientos-module">
+            <div><img src="assets/img/slider/slider-1920-03.jpg" alt="Alojamientos en Paraíso Chuí"></div>
+            <div>
+                <h1>Monoambientes para 2 personas</h1>
+                <p>Perfectos para una escapada romántica o un viaje en pareja, nuestros monoambientes están equipados con todas las comodidades que necesita para una estancia agradable.</p>
+            </div>
+        </div>
+        <div class="alojamientos-module">
+            <div><img src="assets/img/slider/slider-1920-03.jpg" alt="Alojamientos en Paraíso Chuí"></div>
+            <div>
+                <h1>Monoambientes para 2 personas</h1>
+                <p>Perfectos para una escapada romántica o un viaje en pareja, nuestros monoambientes están equipados con todas las comodidades que necesita para una estancia agradable.</p>
+            </div>
+        </div>
+
+    </section>
+</main>
