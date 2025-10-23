58049714d630d16ce1e5cad6e23506b6771b8a2b
GlauciusZacher <glaucius.zacher@gmail.com>
2025-10-23 15:46:04 -0300

    Resized home scrset

--- public_html/app/home.php
+++ public_html/app/home.php
@@ -10,9 +10,13 @@
             <div class="swiper-slide">
                 <div>
                     <picture>
-                        <source media="(max-width: 600px)" srcset="assets/img/slider/slider-1920-01_cr.jpg">
+                        <source media="(max-width: 400px)" srcset="assets/img/slider/slider-400-01.jpg">
+                        <source media="(max-width: 500px)" srcset="assets/img/slider/slider-500-01.jpg">
+                        <source media="(max-width: 600px)" srcset="assets/img/slider/slider-600-01.jpg">
+                        <source media="(max-width: 648px)" srcset="assets/img/slider/slider-648-01.jpg">
                         <source media="(max-width: 700px)" srcset="assets/img/slider/slider-700-01.jpg">
                         <source media="(max-width: 1024px)" srcset="assets/img/slider/slider-1024-01.jpg">
+                        <source media="(max-width: 1280px)" srcset="assets/img/slider/slider-1280-01.jpg">
                         <img src="assets/img/slider/slider-1920-01.jpg" alt="A 100 metros del centro de Barra do Chuí">
                     </picture>
                     <div class="text">
@@ -23,9 +27,13 @@
             <div class="swiper-slide">
                 <div>
                     <picture>
-                        <source media="(max-width: 600px)" srcset="assets/img/slider/slider-1920-02_cr.jpg">
+                        <source media="(max-width: 400px)" srcset="assets/img/slider/slider-400-02.jpg">
+                        <source media="(max-width: 500px)" srcset="assets/img/slider/slider-500-02.jpg">
+                        <source media="(max-width: 600px)" srcset="assets/img/slider/slider-600-02.jpg">
+                        <source media="(max-width: 648px)" srcset="assets/img/slider/slider-648-02.jpg">
                         <source media="(max-width: 700px)" srcset="assets/img/slider/slider-700-02.jpg">
                         <source media="(max-width: 1024px)" srcset="assets/img/slider/slider-1024-02.jpg">
+                        <source media="(max-width: 1280px)" srcset="assets/img/slider/slider-1280-02.jpg">
                         <img src="assets/img/slider/slider-1920-02.jpg" alt="Patio cerrado para su seguridad y comodidad">
                     </picture>
                     <div class="text">
@@ -36,9 +44,13 @@
             <div class="swiper-slide">
                 <div>
                     <picture>
-                        <source media="(max-width: 600px)" srcset="assets/img/slider/slider-1920-03_cr.jpg">
+                        <source media="(max-width: 400px)" srcset="assets/img/slider/slider-400-03.jpg">
+                        <source media="(max-width: 500px)" srcset="assets/img/slider/slider-500-03.jpg">
+                        <source media="(max-width: 600px)" srcset="assets/img/slider/slider-600-03.jpg">
+                        <source media="(max-width: 648px)" srcset="assets/img/slider/slider-648-03.jpg">
                         <source media="(max-width: 700px)" srcset="assets/img/slider/slider-700-03.jpg">
                         <source media="(max-width: 1024px)" srcset="assets/img/slider/slider-1024-03.jpg">
+                        <source media="(max-width: 1280px)" srcset="assets/img/slider/slider-1280-03.jpg">
                         <img src="assets/img/slider/slider-1920-03.jpg" alt="Amplia área verde con parrilleros para disfrutar en familia">
                     </picture>
                     <div class="text">
@@ -49,9 +61,13 @@
             <div class="swiper-slide">
                 <div>
                     <picture>
-                        <source media="(max-width: 600px)" srcset="assets/img/slider/slider-1920-04_cr.jpg">
+                        <source media="(max-width: 400px)" srcset="assets/img/slider/slider-400-04.jpg">
+                        <source media="(max-width: 500px)" srcset="assets/img/slider/slider-500-04.jpg">
+                        <source media="(max-width: 600px)" srcset="assets/img/slider/slider-600-04.jpg">
+                        <source media="(max-width: 648px)" srcset="assets/img/slider/slider-648-04.jpg">
                         <source media="(max-width: 700px)" srcset="assets/img/slider/slider-700-04.jpg">
                         <source media="(max-width: 1024px)" srcset="assets/img/slider/slider-1024-04.jpg">
+                        <source media="(max-width: 1280px)" srcset="assets/img/slider/slider-1280-04.jpg">
                         <img src="assets/img/slider/slider-1920-04.jpg" alt="Estacionamiento interno para su vehículo">
                     </picture>
                     <div class="text">
@@ -62,9 +78,13 @@
             <div class="swiper-slide">
                 <div>
                     <picture>
-                        <source media="(max-width: 600px)" srcset="assets/img/slider/slider-1920-05_cr.jpg">
+                        <source media="(max-width: 400px)" srcset="assets/img/slider/slider-400-05.jpg">
+                        <source media="(max-width: 500px)" srcset="assets/img/slider/slider-500-05.jpg">
+                        <source media="(max-width: 600px)" srcset="assets/img/slider/slider-600-05.jpg">
+                        <source media="(max-width: 648px)" srcset="assets/img/slider/slider-648-05.jpg">
                         <source media="(max-width: 700px)" srcset="assets/img/slider/slider-700-05.jpg">
                         <source media="(max-width: 1024px)" srcset="assets/img/slider/slider-1024-05.jpg">
+                        <source media="(max-width: 1280px)" srcset="assets/img/slider/slider-1280-05.jpg">
                         <img src="assets/img/slider/slider-1920-05.jpg" alt="Confortables alojamientos para una estadía inolvidable">
                     </picture>
                     <div class="text">
@@ -75,9 +95,13 @@
             <div class="swiper-slide">
                 <div>
                     <picture>
-                        <source media="(max-width: 600px)" srcset="assets/img/slider/slider-1920-06_cr.jpg">
+                        <source media="(max-width: 400px)" srcset="assets/img/slider/slider-400-06.jpg">
+                        <source media="(max-width: 500px)" srcset="assets/img/slider/slider-500-06.jpg">
+                        <source media="(max-width: 600px)" srcset="assets/img/slider/slider-600-06.jpg">
+                        <source media="(max-width: 648px)" srcset="assets/img/slider/slider-648-06.jpg">
                         <source media="(max-width: 700px)" srcset="assets/img/slider/slider-700-06.jpg">
                         <source media="(max-width: 1024px)" srcset="assets/img/slider/slider-1024-06.jpg">
+                        <source media="(max-width: 1280px)" srcset="assets/img/slider/slider-1280-06.jpg">
                         <img src="assets/img/slider/slider-1920-06.jpg" alt="Un oasis de tranquilidad para tus vacaciones">
                     </picture>
                     <div class="text">
