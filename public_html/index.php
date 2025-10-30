3327102c5fcad1cb67d91da7eb0a5cfd0a4f9f95
GlauciusZacher <glaucius.zacher@gmail.com>
2025-10-30 17:42:04 -0300

    Favicon added

--- public_html/index.php
+++ public_html/index.php
@@ -31,6 +31,10 @@ if (array_key_exists($page, $links)) {
     <!-- <link rel="stylesheet" href="assets/swiper/swiper-bundle.min.css" />
     <link rel="stylesheet" href="assets/css/main.css"> -->
     <title>Paraíso Chuí</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="assets/img/favicon/site.webmanifest">
 </head>
 
 <body>
